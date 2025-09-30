nixops4/nixops4

feat: Add nix overrides to make bindgen work

flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    nix.url = "github:tweag/nix/nix-c-bindings";
+    nix.inputs.nixpkgs.follows = "nixpkgs";
     nix-cargo-integration.url = "github:yusdacra/nix-cargo-integration";
     nix-cargo-integration.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -24,6 +26,11 @@
         perSystem = { config, self', inputs', pkgs, ... }: {
 
           packages.default = config.packages.nixops4-release;
+          packages.nix = inputs'.nix.packages.nix.overrideAttrs {
+            # checkPhase does not seem to terminate.
+            # TODO: remove override
+            doCheck = false;
+          };
 
           pre-commit.settings.hooks.nixpkgs-fmt.enable = true;
           pre-commit.settings.hooks.rustfmt.enable = true;
@@ -31,16 +38,29 @@
           pre-commit.settings.hooks.rustfmt.entry = lib.mkForce "${pkgs.rustfmt}/bin/cargo-fmt fmt --all --manifest-path ./rust/Cargo.toml -- --color always";
 
           devShells.default = pkgs.mkShell {
-            inputsFrom = [ config.nci.outputs.nixops4.devShell ];
+            name = "nixops4-devshell";
+            strictDeps = true;
+            inputsFrom = [ config.nci.outputs.nixops4-project.devShell ];
+            inherit (config.nci.outputs.nixops4-project.devShell.env)
+              LIBCLANG_PATH
+              BINDGEN_EXTRA_CLANG_ARGS
+              ;
+            buildInputs = [
+              config.packages.nix
+            ];
             nativeBuildInputs = [
               pkgs.rust-analyzer
               pkgs.nixpkgs-fmt
               pkgs.rustfmt
+              pkgs.pkg-config
+              pkgs.clang-tools # clangd
             ];
             shellHook = ''
               ${config.pre-commit.installationScript}
               echo 1>&2 "Welcome to the development shell!"
             '';
+            # rust-analyzer needs a NIX_PATH for some reason
+            NIX_PATH = "nixpkgs=${inputs.nixpkgs}";
           };
         };
         flake = { };

rust/nci.nix
@@ -1,6 +1,33 @@
 {
-  perSystem = { pkgs, config, ... }: {
+  perSystem = { lib, config, pkgs, ... }: {
     # https://flake.parts/options/nix-cargo-integration
-    nci.projects.nixops4.path = ./.;
+    nci.projects.nixops4-project = {
+      path = ./.;
+      drvConfig = {
+        mkDerivation = {
+          buildInputs = [
+            config.packages.nix
+            # stdbool.h
+            pkgs.stdenv.cc
+          ];
+          nativeBuildInputs = [
+            pkgs.pkg-config
+          ];
+        };
+        # NOTE: duplicated in flake.nix devShell
+        env = {
+          LIBCLANG_PATH =
+            if pkgs.stdenv.cc.isClang then
+              null # don't set the variable
+            else
+              lib.makeLibraryPath [ pkgs.buildPackages.llvmPackages.clang-unwrapped ];
+          BINDGEN_EXTRA_CLANG_ARGS =
+            if pkgs.stdenv.cc.isClang then
+              null # don't set the variable
+            else
+              "-I${lib.getDev pkgs.stdenv.cc.cc}/lib/gcc/${pkgs.stdenv.hostPlatform.config}/${pkgs.stdenv.cc.cc.version}/include";
+        };
+      };
+    };
   };
 }

nixops4/nix-bindings-rust

(cherry picked from commit 7caeec86a016fc7427d71ac3713adb2293c2e1ce)

flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    nix.url = "github:tweag/nix/nix-c-bindings";
+    nix.inputs.nixpkgs.follows = "nixpkgs";
     nix-cargo-integration.url = "github:yusdacra/nix-cargo-integration";
     nix-cargo-integration.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -23,22 +25,42 @@
         systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
         perSystem = { config, self', inputs', pkgs, ... }: {
 
+
+          packages.nix = inputs'.nix.packages.nix.overrideAttrs {
+            # checkPhase does not seem to terminate.
+            # TODO: remove override
+            doCheck = false;
+          };
+
           pre-commit.settings.hooks.nixpkgs-fmt.enable = true;
           pre-commit.settings.hooks.rustfmt.enable = true;
           # Override to pass `--all`
           pre-commit.settings.hooks.rustfmt.entry = lib.mkForce "${pkgs.rustfmt}/bin/cargo-fmt fmt --all --manifest-path ./rust/Cargo.toml -- --color always";
 
           devShells.default = pkgs.mkShell {
+            name = "nix-bindings-devshell";
+            strictDeps = true;
             inputsFrom = [ config.nci.outputs.nix-bindings.devShell ];
+            inherit (config.nci.outputs.nix-bindings.devShell.env)
+              LIBCLANG_PATH
+              BINDGEN_EXTRA_CLANG_ARGS
+              ;
+            buildInputs = [
+              config.packages.nix
+            ];
             nativeBuildInputs = [
               pkgs.rust-analyzer
               pkgs.nixpkgs-fmt
               pkgs.rustfmt
+              pkgs.pkg-config
+              pkgs.clang-tools # clangd
             ];
             shellHook = ''
               ${config.pre-commit.installationScript}
               echo 1>&2 "Welcome to the development shell!"
             '';
+            # rust-analyzer needs a NIX_PATH for some reason
+            NIX_PATH = "nixpkgs=${inputs.nixpkgs}";
           };
         };
         flake = { };

rust/nci.nix
@@ -1,6 +1,33 @@
 {
-  perSystem = { pkgs, config, ... }: {
+  perSystem = { lib, config, pkgs, ... }: {
     # https://flake.parts/options/nix-cargo-integration
-    nci.projects.nix-bindings.path = ./.;
+    nci.projects.nix-bindings = {
+      path = ./.;
+      drvConfig = {
+        mkDerivation = {
+          buildInputs = [
+            config.packages.nix
+            # stdbool.h
+            pkgs.stdenv.cc
+          ];
+          nativeBuildInputs = [
+            pkgs.pkg-config
+          ];
+        };
+        # NOTE: duplicated in flake.nix devShell
+        env = {
+          LIBCLANG_PATH =
+            if pkgs.stdenv.cc.isClang then
+              null # don't set the variable
+            else
+              lib.makeLibraryPath [ pkgs.buildPackages.llvmPackages.clang-unwrapped ];
+          BINDGEN_EXTRA_CLANG_ARGS =
+            if pkgs.stdenv.cc.isClang then
+              null # don't set the variable
+            else
+              "-I${lib.getDev pkgs.stdenv.cc.cc}/lib/gcc/${pkgs.stdenv.hostPlatform.config}/${pkgs.stdenv.cc.cc.version}/include";
+        };
+      };
+    };
   };
 }