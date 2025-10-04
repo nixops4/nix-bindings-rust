nixops4/nixops4

feat: Value::new_borrowed

rust/nix-expr/src/value.rs
@@ -54,11 +54,24 @@ pub struct Value {
     inner: NonNull<raw::Value>,
 }
 impl Value {
+    /// Take ownership of a new Value.
+    ///
+    /// This does not call `nix_gc_incref`, but does call `nix_gc_decref` when dropped.
     pub(crate) fn new(inner: *mut raw::Value) -> Self {
         Value {
             inner: NonNull::new(inner).unwrap(),
         }
     }
+
+    /// Borrow a reference to a Value.
+    ///
+    /// This calls `nix_gc_incref`, and the returned Value will call `nix_gc_decref` when dropped.
+    pub(crate) fn new_borrowed(inner: *mut raw::Value) -> Self {
+        let v = Value::new(inner);
+        unsafe { raw::gc_incref(null_mut(), inner) };
+        v
+    }
+
     pub(crate) fn raw_ptr(&self) -> *mut raw::Value {
         self.inner.as_ptr()
     }

nixops4/nix-bindings-rust

(cherry picked from commit 86e3828250d4de15f9cf49d19222eb382c77f7e2)

rust/nix-expr/src/value.rs
@@ -54,11 +54,24 @@ pub struct Value {
     inner: NonNull<raw::Value>,
 }
 impl Value {
+    /// Take ownership of a new Value.
+    ///
+    /// This does not call `nix_gc_incref`, but does call `nix_gc_decref` when dropped.
     pub(crate) fn new(inner: *mut raw::Value) -> Self {
         Value {
             inner: NonNull::new(inner).unwrap(),
         }
     }
+
+    /// Borrow a reference to a Value.
+    ///
+    /// This calls `nix_gc_incref`, and the returned Value will call `nix_gc_decref` when dropped.
+    pub(crate) fn new_borrowed(inner: *mut raw::Value) -> Self {
+        let v = Value::new(inner);
+        unsafe { raw::value_incref(null_mut(), inner) };
+        v
+    }
+
     pub(crate) fn raw_ptr(&self) -> *mut raw::Value {
         self.inner.as_ptr()
     }