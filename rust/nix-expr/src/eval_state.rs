Radvendii/nixops4

feat: EvalState.require_int

rust/nix-expr/src/eval_state.rs
@@ -1,4 +1,4 @@
-use crate::value::{Value, ValueType};
+use crate::value::{Int, Value, ValueType};
 use anyhow::Context as _;
 use anyhow::{bail, Result};
 use lazy_static::lazy_static;
@@ -116,6 +116,15 @@ impl EvalState {
         let r = unsafe { raw::nix_get_type(self.context.ptr(), value.raw_ptr()) };
         Ok(ValueType::from_raw(r))
     }
+    pub fn require_int(&self, v: &Value) -> Result<Int> {
+        let t = self.value_type(v).unwrap();
+        if t != ValueType::Int {
+            bail!("expected an int, but got a {:?}", t);
+        }
+        let i = unsafe { raw::nix_get_int(self.context.ptr(), v.raw_ptr()) };
+        Ok(i)
+    }
+
     /// Not exposed, because the caller must always explicitly handle the context or not accept one at all.
     fn get_string(&self, value: &Value) -> Result<String> {
         let c_str_raw = unsafe { raw::nix_get_string(self.context.ptr(), value.raw_ptr()) };
@@ -290,6 +299,21 @@ mod tests {
             es.force(&v).unwrap();
             let t = es.value_type(&v).unwrap();
             assert!(t == ValueType::Bool);
+        })
+        .unwrap();
+    }
+
+    #[test]
+    fn eval_state_value_int() {
+        gc_registering_current_thread(|| {
+            let store = Store::open("auto").unwrap();
+            let es = EvalState::new(store).unwrap();
+            let v = es.eval_from_string("1", "<test>").unwrap();
+            es.force(&v).unwrap();
+            let t = es.value_type(&v).unwrap();
+            assert!(t == ValueType::Int);
+            let i = es.require_int(&v).unwrap();
+            assert!(i == 1);
         })
         .unwrap();
     }

nixops4/nix-bindings-rust

(cherry picked from commit 890441adf963f1a33ba75889cc9735deeefbf51c)

rust/nix-expr/src/eval_state.rs
@@ -1,4 +1,4 @@
-use crate::value::{Value, ValueType};
+use crate::value::{Int, Value, ValueType};
 use anyhow::Context as _;
 use anyhow::{bail, Result};
 use lazy_static::lazy_static;
@@ -115,6 +115,15 @@ impl EvalState {
         let r = unsafe { raw::get_type(self.context.ptr(), value.raw_ptr()) };
         Ok(ValueType::from_raw(r))
     }
+    pub fn require_int(&self, v: &Value) -> Result<Int> {
+        let t = self.value_type(v).unwrap();
+        if t != ValueType::Int {
+            bail!("expected an int, but got a {:?}", t);
+        }
+        let i = unsafe { raw::get_int(self.context.ptr(), v.raw_ptr()) };
+        Ok(i)
+    }
+
     /// Not exposed, because the caller must always explicitly handle the context or not accept one at all.
     fn get_string(&self, value: &Value) -> Result<String> {
         let mut r = result_string_init!();
@@ -298,6 +307,21 @@ mod tests {
             es.force(&v).unwrap();
             let t = es.value_type(&v).unwrap();
             assert!(t == ValueType::Bool);
+        })
+        .unwrap();
+    }
+
+    #[test]
+    fn eval_state_value_int() {
+        gc_registering_current_thread(|| {
+            let store = Store::open("auto").unwrap();
+            let es = EvalState::new(store).unwrap();
+            let v = es.eval_from_string("1", "<test>").unwrap();
+            es.force(&v).unwrap();
+            let t = es.value_type(&v).unwrap();
+            assert!(t == ValueType::Int);
+            let i = es.require_int(&v).unwrap();
+            assert!(i == 1);
         })
         .unwrap();
     }