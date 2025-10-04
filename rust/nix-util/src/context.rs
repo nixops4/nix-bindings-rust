nixops4/nixops4

feat: Context.is_key_error

rust/nix-util/src/context.rs
@@ -32,6 +32,10 @@ impl Context {
         }
         Ok(())
     }
+    /// NIX_ERR_KEY is returned when e.g. an attribute is missing. Return true if the error is of this type.
+    pub fn is_key_error(&self) -> bool {
+        unsafe { raw::nix_err_code(self.inner.as_ptr()) == raw::NIX_ERR_KEY }
+    }
 }
 
 impl Drop for Context {

nixops4/nix-bindings-rust

(cherry picked from commit f754e243852103164dbc6fdaf8c0cccb3c85cfb1)

rust/nix-util/src/context.rs
@@ -34,6 +34,10 @@ impl Context {
         }
         Ok(())
     }
+    /// NIX_ERR_KEY is returned when e.g. an attribute is missing. Return true if the error is of this type.
+    pub fn is_key_error(&self) -> bool {
+        unsafe { raw::err_code(self.inner.as_ptr()) == raw::NIX_ERR_KEY }
+    }
 }
 
 impl Drop for Context {